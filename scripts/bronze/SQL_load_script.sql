125ae4a9f978d2a602f7a2e92e32514afcd9522d
Ayoub Youhad <ayoubyouhad79@gmail.com>
2025-12-26 20:49:26 +0100

    refactor: Convert bronze data load script into stored procedure

--- scripts/bronze/SQL_load_script.sql
+++ scripts/bronze/SQL_load_script.sql
@@ -1,70 +1,60 @@
-USE DataWarehouse;
-GO
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
 
-TRUNCATE TABLE bronze.crm_cust_info;
-GO
+	TRUNCATE TABLE bronze.crm_cust_info;
+	BULK INSERT bronze.crm_cust_info
+	FROM 'C:\Users\hp\Documents\ENSET\Big Data\Projects\SQL Data Warehouse\datasets\source_crm\cust_info.csv'
+	WITH (
+	   FIRSTROW = 2,
+	   FIELDTERMINATOR = ',',
+	   TABLOCK
+	);
 
-BULK INSERT bronze.crm_cust_info
-FROM 'C:\Users\hp\Documents\ENSET\Big Data\Projects\SQL Data Warehouse\datasets\source_crm\cust_info.csv'
-WITH (
-   FIRSTROW = 2,
-   FIELDTERMINATOR = ',',
-   TABLOCK
-);
+	TRUNCATE TABLE bronze.crm_prd_info;
+	BULK INSERT bronze.crm_prd_info
+	FROM 'C:\Users\hp\Documents\ENSET\Big Data\Projects\SQL Data Warehouse\datasets\source_crm\prd_info.csv'
+	WITH (
+	   FIRSTROW = 2,
+	   FIELDTERMINATOR = ',',
+	   TABLOCK
+	)
 
-TRUNCATE TABLE bronze.crm_prd_info;
-GO
-
-BULK INSERT bronze.crm_prd_info
-FROM 'C:\Users\hp\Documents\ENSET\Big Data\Projects\SQL Data Warehouse\datasets\source_crm\prd_info.csv'
-WITH (
-   FIRSTROW = 2,
-   FIELDTERMINATOR = ',',
-   TABLOCK
-)
-
-TRUNCATE TABLE bronze.crm_sales_details;
-GO
-
-BULK INSERT bronze.crm_sales_details
-FROM 'C:\Users\hp\Documents\ENSET\Big Data\Projects\SQL Data Warehouse\datasets\source_crm\sales_details.csv'
-WITH (
-   FIRSTROW = 2,
-   FIELDTERMINATOR = ',',
-   TABLOCK
-)
+	TRUNCATE TABLE bronze.crm_sales_details;
+	BULK INSERT bronze.crm_sales_details
+	FROM 'C:\Users\hp\Documents\ENSET\Big Data\Projects\SQL Data Warehouse\datasets\source_crm\sales_details.csv'
+	WITH (
+	   FIRSTROW = 2,
+	   FIELDTERMINATOR = ',',
+	   TABLOCK
+	)
 
 
-TRUNCATE TABLE bronze.erp_cust_az12;
-GO
+	TRUNCATE TABLE bronze.erp_cust_az12;
+	BULK INSERT bronze.erp_cust_az12
+	FROM 'C:\Users\hp\Documents\ENSET\Big Data\Projects\SQL Data Warehouse\datasets\source_erp\cust_az12.csv'
+	WITH (
+	   FIRSTROW = 2,
+	   FIELDTERMINATOR = ',',
+	   TABLOCK
+	)
 
-BULK INSERT bronze.erp_cust_az12
-FROM 'C:\Users\hp\Documents\ENSET\Big Data\Projects\SQL Data Warehouse\datasets\source_erp\cust_az12.csv'
-WITH (
-   FIRSTROW = 2,
-   FIELDTERMINATOR = ',',
-   TABLOCK
-)
+	TRUNCATE TABLE bronze.erp_loc_a101;
+	BULK INSERT bronze.erp_loc_a101
+	FROM 'C:\Users\hp\Documents\ENSET\Big Data\Projects\SQL Data Warehouse\datasets\source_erp\loc_a101.csv'
+	WITH (
+	   FIRSTROW = 2,
+	   FIELDTERMINATOR = ',',
+	   TABLOCK
+	)
 
-TRUNCATE TABLE bronze.erp_loc_a101;
-GO
+	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+	BULK INSERT bronze.erp_px_cat_g1v2
+	FROM 'C:\Users\hp\Documents\ENSET\Big Data\Projects\SQL Data Warehouse\datasets\source_erp\px_cat_g1v2.csv'
+	WITH (
+	   FIRSTROW = 2,
+	   FIELDTERMINATOR = ',',
+	   TABLOCK
+	);
 
-BULK INSERT bronze.erp_loc_a101
-FROM 'C:\Users\hp\Documents\ENSET\Big Data\Projects\SQL Data Warehouse\datasets\source_erp\loc_a101.csv'
-WITH (
-   FIRSTROW = 2,
-   FIELDTERMINATOR = ',',
-   TABLOCK
-)
-
-TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-GO
-
-BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'C:\Users\hp\Documents\ENSET\Big Data\Projects\SQL Data Warehouse\datasets\source_erp\px_cat_g1v2.csv'
-WITH (
-   FIRSTROW = 2,
-   FIELDTERMINATOR = ',',
-   TABLOCK
-)
+END
 
