8c9afe9946f3bb18694f287b9e37b054d86c3495
Ayoub Youhad <ayoubyouhad79@gmail.com>
2025-12-26 21:09:48 +0100

    feat: Add logging and documentation to bronze load procedure
    - Add section headers for CRM and ERP table loading
    - Add PRINT statements for each TRUNCATE operation
    - Add PRINT statements for each BULK INSERT operation

--- scripts/bronze/SQL_load_script.sql
+++ scripts/bronze/SQL_load_script.sql
@@ -1,7 +1,20 @@
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
 
+    PRINT '=========================================='
+    PRINT 'Loading Bronze Layer'
+    PRINT '=========================================='
+
+
+    PRINT '------------------------------------------'
+    PRINT 'Loading CRM Table'
+    PRINT '------------------------------------------'
+
+
+    PRINT '>> Trunicating Table : bronze.crm_cust_info '
 	TRUNCATE TABLE bronze.crm_cust_info;
+
+	PRINT '>> Inserting Data into : bronze.crm_cust_info '
 	BULK INSERT bronze.crm_cust_info
 	FROM 'C:\Users\hp\Documents\ENSET\Big Data\Projects\SQL Data Warehouse\datasets\source_crm\cust_info.csv'
 	WITH (
@@ -10,7 +23,10 @@ BEGIN
 	   TABLOCK
 	);
 
+	PRINT '>> Trunicating Table : bronze.crm_prd_info '
 	TRUNCATE TABLE bronze.crm_prd_info;
+
+	PRINT '>> Inserting Data into : bronze.crm_prd_info '
 	BULK INSERT bronze.crm_prd_info
 	FROM 'C:\Users\hp\Documents\ENSET\Big Data\Projects\SQL Data Warehouse\datasets\source_crm\prd_info.csv'
 	WITH (
@@ -19,7 +35,10 @@ BEGIN
 	   TABLOCK
 	)
 
+	PRINT '>> Trunicating Table : bronze.crm_sales_details '
 	TRUNCATE TABLE bronze.crm_sales_details;
+
+	PRINT '>> Inserting Data into : bronze.crm_sales_details '
 	BULK INSERT bronze.crm_sales_details
 	FROM 'C:\Users\hp\Documents\ENSET\Big Data\Projects\SQL Data Warehouse\datasets\source_crm\sales_details.csv'
 	WITH (
@@ -28,8 +47,14 @@ BEGIN
 	   TABLOCK
 	)
 
+	PRINT '------------------------------------------'
+    PRINT 'Loading ERP Table'
+    PRINT '------------------------------------------'
 
+	PRINT '>> Trunicating Table : bronze.erp_cust_az12 '
 	TRUNCATE TABLE bronze.erp_cust_az12;
+
+	PRINT '>> Inserting Data into : bronze.erp_cust_az12 '
 	BULK INSERT bronze.erp_cust_az12
 	FROM 'C:\Users\hp\Documents\ENSET\Big Data\Projects\SQL Data Warehouse\datasets\source_erp\cust_az12.csv'
 	WITH (
@@ -38,7 +63,10 @@ BEGIN
 	   TABLOCK
 	)
 
+	PRINT '>> Trunicating Table : bronze.erp_loc_a101'
 	TRUNCATE TABLE bronze.erp_loc_a101;
+
+	PRINT '>> Inserting Data into : bronze.erp_loc_a101'
 	BULK INSERT bronze.erp_loc_a101
 	FROM 'C:\Users\hp\Documents\ENSET\Big Data\Projects\SQL Data Warehouse\datasets\source_erp\loc_a101.csv'
 	WITH (
@@ -47,7 +75,10 @@ BEGIN
 	   TABLOCK
 	)
 
+	PRINT '>> Trunicating Table : bronze.erp_px_cat_g1v2'
 	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+	PRINT '>> Inserting Data into : bronze.erp_px_cat_g1v2'
 	BULK INSERT bronze.erp_px_cat_g1v2
 	FROM 'C:\Users\hp\Documents\ENSET\Big Data\Projects\SQL Data Warehouse\datasets\source_erp\px_cat_g1v2.csv'
 	WITH (
