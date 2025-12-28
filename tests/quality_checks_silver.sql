4f4d1476381d9f2f6f07dd68ba9f58c5370a5817
Ayoub Youhad <ayoubyouhad79@gmail.com>
2025-12-28 23:07:16 +0100

    test: Add date logic validation for sales transactions
    - Validate order date is not after ship date
    - Validate order date is not after due date

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -114,3 +114,12 @@ WHERE  sls_due_dt  <=0  OR
        LEN(sls_due_dt) !=8 OR
        sls_due_dt > 20500101 OR
        sls_due_dt < 19000101
+
+-- Check for Invalid Date Orders (Order Date > Shipping/Due Dates)
+-- Expectation: No Results
+
+SELECT 
+    * 
+FROM bronze.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt 
+   OR sls_order_dt > sls_due_dt;
