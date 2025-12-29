9b8682516f58eea777a588ab20b03f4e1e768b6a
Ayoub Youhad <ayoubyouhad79@gmail.com>
2025-12-29 18:24:31 +0100

    refactor: Convert customer master query into gold dimension view
    - Wrap existing customer merge query into gold.dim_customers view
    - Add surrogate key (customer_key) using ROW_NUMBER for star schema

--- scripts/03-gold/ddl_gold.sql
+++ scripts/03-gold/ddl_gold.sql
@@ -1,12 +1,18 @@
-SELECT 
 
+-- =============================================================================
+-- Create Dimension: gold.dim_customers
+-- =============================================================================
+
+CREATE VIEW gold.dim_customers AS
+SELECT 
+    ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, -- Surrogate key
 	ci.cst_id              AS customer_id,
 	ci.cst_key             AS customer_number,
 	ci.cst_firstname       AS first_name,
 	ci.cst_lastname        AS last_name,
 	CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is The Master Of the Gender Info
-	      ELSE COALESCE(ca.gen,	'n/a')
-	END gender,
+	      ELSE COALESCE(ca.gen,	'n/a') -- Fallback to ERP data
+	END                    AS gender,
 	ci.cst_marital_status  AS marital_status ,
 	la.cntry               AS country,
 	ca.bdate               AS birthday,
@@ -17,3 +23,4 @@ LEFT JOIN silver.erp_cust_az12 ca
 ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 la
 ON ci.cst_key = la.cid
+GO
