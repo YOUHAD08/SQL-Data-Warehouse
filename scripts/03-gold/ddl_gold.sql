de4c97330c59d2b1e3513f7ecfcaf5f1d570577b
Ayoub Youhad <ayoubyouhad79@gmail.com>
2025-12-29 18:03:00 +0100

    refactor: Add business-friendly column aliases to customer master query

--- scripts/03-gold/ddl_gold.sql
+++ scripts/03-gold/ddl_gold.sql
@@ -1,15 +1,17 @@
 SELECT 
-	ci.cst_id,
-	ci.cst_key,
-	ci.cst_firstname,
-	ci.cst_lastname,
-	ci.cst_marital_status,
+
+	ci.cst_id              AS customer_id,
+	ci.cst_key             AS customer_number,
+	ci.cst_firstname       AS first_name,
+	ci.cst_lastname        AS last_name,
 	CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is The Master Of the Gender Info
 	      ELSE COALESCE(ca.gen,	'n/a')
-	END new_gen,
-	ci.cst_create_date,
-	ca.bdate,
-	la.cntry
+	END gender,
+	ci.cst_marital_status  AS marital_status ,
+	la.cntry               AS country,
+	ca.bdate               AS birthday,
+	ci.cst_create_date     AS create_date
+	
 FROM silver.crm_cust_info ci
 LEFT JOIN silver.erp_cust_az12 ca
 ON ci.cst_key = ca.cid
