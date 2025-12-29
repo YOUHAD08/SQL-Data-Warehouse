a92c8283bb70baded0e394509403d9d6525e1219
Ayoub Youhad <ayoubyouhad79@gmail.com>
2025-12-29 18:26:22 +0100

    test: Add data quality checks for gold.dim_customers dimension
    - Validate gender values for consistency and standardization
    - Add final data review query for manual inspection

---
+++ tests/quality_checks_gold.sql
@@ -0,0 +1,10 @@
+-- ====================================================================
+-- Checking 'gold.dim_customers'
+-- ====================================================================
+
+SELECT 
+DISTINCT gender
+FROM  gold.dim_customers;
+
+-- Final Check
+SELECT * FROM  gold.dim_customers;
